aa8b6c02d75e529fbe731cbcc84b23a0e4ae92cf
Obalix <axel.eckenberger@gmail.com>
2021-04-06 21:08:32 +0200

    Set assembly version to 3.0. Updated .nuspec file.

--- NGeo2/Properties/AssemblyInfo.cs
+++ NGeo2/Properties/AssemblyInfo.cs
@@ -1,17 +1,17 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NGeo.NET40")]
+[assembly: AssemblyTitle("NGeo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NGeo.NET40")]
-[assembly: AssemblyCopyright("Copyright © Axel Eckenberger 2016")]
+[assembly: AssemblyProduct("NGeo")]
+[assembly: AssemblyCopyright("Copyright © Axel Eckenberger 2016-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -20,22 +20,11 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("NGeo2.Tests.NET40")]
+[assembly: InternalsVisibleTo("NGeo2.Tests")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f503720a-bf27-461f-8b73-79bce83a6d87")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
 
